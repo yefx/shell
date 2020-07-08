2ba1bcb0bc9612931c0632c02d22b5b0e0775a3d
yefx <43061047+yefx@users.noreply.github.com>
2020-07-08 23:19:54 +0800

    Update time.sh

--- time.sh
+++ time.sh
@@ -1,10 +1,13 @@
 #!/bin/bash
-i=`date "+%M"`
+Time=`date +%M`
+i=`echo ${Time} | awk '{printf("%d\n",$1)}'`
 while(($i<60))
 do
    if (($i%5 != 0))
      then
      echo $i
+     date "+%Y%m%d"
+     ls /etc/nginx
      break
    fi
    i=`date "+%M"`
